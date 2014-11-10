fe8822593c25a64e5e922306de0e9fc4d16f04e0
Søren Howe Gersager <syrelyre@gmail.com>
2014-11-10 14:27:47 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -17,13 +17,18 @@ class ToxTestCommand(TestCommand):
         sys.exit(os.system('tox'))
 
 setup(
+    name="sentimentube",
+    version="0.1.0",
+    author="Søren Howe Gersager, Anders Lundberg Rahbek",
+    author_email="s094557@student.dtu.dk, s107029@student.dtu.dk",
+    description="Youtube sentiment analysis with webinterface",
     license="MIT",
     keywords="youtube sentiment analysis",
     url="https://github.com/syre/datamining-with-python",
-    # packages=find_packages(),
     packages=["sentimentube"],
     long_description=open('README.md').read(),
-    install_requires=["requests"],
+    install_requires=["requests", "flask", "matplotlib", "sqlite3"],
+    py_modules=["sentimentube"]
     cmdclass={"test": ToxTestCommand},
     tests_require=["tox"],
     classifiers=[
