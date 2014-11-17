032a710ebac84f42effe7b4845841de9f7632a70
Søren Howe Gersager <syrelyre@gmail.com>
2014-11-17 12:51:28 +0100

    pep-8 compliance

--- test/test_flask.py
+++ test/test_flask.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from unittest import mock, TestCase
+from unittest import TestCase
 import webserve
-import youtube
-import unittest
-import database
+
 
 class WebServeTestCase(TestCase):
     def setUp(self):
         webserve.app.config["TESTING"] = True
         self.app = webserve.app.test_client()
-
 
     def test_start_page_load_correct(self):
         response = self.app.get("/")
@@ -18,7 +15,9 @@ class WebServeTestCase(TestCase):
 
     def test_video_page_load_correct(self):
         response = self.app.get("/video?video_id={}".format("oavMtUWDBTM"))
-        assert "Analysis of video with ID: oavMtUWDBTM" in response.data.decode("utf-8")
+
+        assert "Analysis of video with ID: oavMtUWDBTM" in
+        response.data.decode("utf-8")
 
     def test_video_page_load_error_wrong_id(self):
         response = self.app.get("/video?video_id={}".format("wrong_id"))
